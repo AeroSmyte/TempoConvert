ecf2911c720d79dc298abcfade57e43995b03f3e
Lexi McQueen <alexismcqueen01@gmail.com>
2023-07-23 17:34:01 -0400

    Initial Commit

---
+++ Tempo/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Tempo
+//
+//  Created by Lexi McQueen on 7/23/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Tempo/TempoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TempoApp.swift
+//  Tempo
+//
+//  Created by Lexi McQueen on 7/23/23.
+//
+
+import SwiftUI
+
+@main
+struct TempoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
