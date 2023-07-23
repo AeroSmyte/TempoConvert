d2e2497d7de4c1a56f8257816a00fbde4980c103
Lexi McQueen <alexismcqueen01@gmail.com>
2023-07-23 18:41:54 -0400

    Created the temperature conversion app content.

--- Tempo/ContentView.swift
+++ Tempo/ContentView.swift
@@ -7,15 +7,60 @@
 
 import SwiftUI
 
+enum ConversionType: String, CaseIterable, Identifiable {
+    case Fahrenheit, Celsius
+    
+    var id: Self { self }
+}
+
 struct ContentView: View {
+    @State private var inputTemperature = 60
+    @State private var selectedConversion : ConversionType = .Fahrenheit
+    
+    var convertedFahrenheitTemperature: Double {
+        let fahrenheitTemperature = (Double(inputTemperature) - 32.0) / 1.8
+        return fahrenheitTemperature
+    }
+    
+    var convertedCelsuisTemperature: Double {
+        let celsiusTemperature = (Double(inputTemperature) * 1.8) + 32.0
+        return celsiusTemperature
+    }
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            Form {
+
+                Section {
+                    Picker("Choose a Unit:", selection: $selectedConversion) {
+                        Text("Fahrenheit").tag(ConversionType.Fahrenheit)
+                        Text("Celsuis").tag(ConversionType.Celsius)
+                    }
+                }
+                
+                Section {
+                    TextField("Amount: ", value: $inputTemperature, format: .number)
+                } header: {
+                    if selectedConversion == .Celsius {
+                        Text("Input Celsius Temperature")
+                    }
+                    Text("Input Fahrenheit Temperature")
+
+                }
+                
+                Section {
+                    if selectedConversion == .Celsius {
+                        Text(convertedCelsuisTemperature, format: .number)
+                    } else {
+                        Text(convertedFahrenheitTemperature, format: .number)
+                    }
+                } header: {
+                    Text("Converted Temperature")
+                }
+            }
+            .navigationTitle("Temp Convert")
         }
-        .padding()
+        
     }
 }
 
